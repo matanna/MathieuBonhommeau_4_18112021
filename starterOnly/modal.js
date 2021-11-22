5c08806f4ad11cff99b3c482893b042c706c12b7
matanna <mat.bonhommeau85@gmail.com>
2021-11-22 12:39:04 +0100

    Implement close modal feature with X btn and create animations for hide it smoothly

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close");
+const modalContent = document.querySelector(".content");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +20,25 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  modalbg.classList.remove("hide-bground");
+  modalContent.classList.remove("hide-content");
+}
+
+//close modal event
+modalClose.addEventListener('click', closeModal);
+
+//close modal form
+function closeModal() {
+
+  //Retrieve delay of animation from css variable, remove the 's' and change number in millisecond
+  const animationDuration = (getComputedStyle(modalContent).animationDuration.replace('s', '')) * 1000;
+
+  //Add 2 class for bground and content for apply differents animations
+  modalbg.classList.add("hide-bground");
+  modalContent.classList.add("hide-content");
+    
+  //Apply a delay (animationDuration) for hide modal
+  setTimeout(() => modalbg.style.display = "none", animationDuration);
 }
 
 
